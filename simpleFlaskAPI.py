eda5df598dfcaeb2b57dadbfca0829fbd1ec1225
mixelpixel <mixelpix@mac.com>
2017-03-05 15:38:26 -0500

    basic python script for Flask webpage

--- simpleFlaskAPI.py
+++ simpleFlaskAPI.py
@@ -1,12 +1,20 @@
 # https://youtu.be/KucNCe_vgcU
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 # 'render_template' renders templates and 'request' handles http requests
-
 import sqlite3
+
+app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template(base.html)
+    return render_template("base.html")
 
 
+
+
+
+
+
+# if __name__ == '__main__':
+#     app.run(debug = True)
